c6b6a064a19653eee6935c31dcc4149e8ff189dc
woollywood <Gerbert.Wells969@yandex.ru>
2023-10-03 18:55:49 +0300

    lenis

--- src/js/libs/scrollController.js
+++ src/js/libs/scrollController.js
@@ -1,42 +1,24 @@
 import { gsap, ScrollTrigger } from 'gsap/all.js';
+import Lenis from '@studio-freight/lenis';
+import '../../scss/libs/lenis.scss';
 
-// don't forget to register plugins
 gsap.registerPlugin(ScrollTrigger);
 
-if (document.querySelector('[data-scroll-container]')) {
-	(async () => {
-		await import('locomotive-scroll/dist/locomotive-scroll.css');
-		let LocomotiveScrollModule = await import('locomotive-scroll');
-		let LocomotiveScroll = LocomotiveScrollModule.default;
-		const smoothScrolling = new LocomotiveScroll({
-			el: document.querySelector('[data-scroll-container]'),
-			smooth: true,
-		});
+const lenis = new Lenis();
 
-		smoothScrolling.on('scroll', ScrollTrigger.update);
-
-		ScrollTrigger.scrollerProxy('[data-scroll-container]', {
-			scrollTop(value) {
-				return arguments.length
-					? smoothScrolling.scrollTo(value, 0, 0)
-					: smoothScrolling.scroll.instance.scroll.y;
-			},
-			getBoundingClientRect() {
-				return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-			},
-			pinType: document.querySelector('[data-scroll-container]').style.transform ? 'transform' : 'fixed',
-		});
-
-		// Init function for code here
-		init();
-
-		ScrollTrigger.addEventListener('refresh', () => smoothScrolling.update());
-		ScrollTrigger.refresh();
-	})();
-} else {
-	init();
+function raf(time) {
+	lenis.raf(time);
+	requestAnimationFrame(raf);
 }
 
-function init() {
-	// Custom code here
-}
+requestAnimationFrame(raf);
+
+// gsap.to('.section', {
+// 	x: -100,
+// 	scrollTrigger: {
+// 		trigger: '.section',
+// 		start: 'top top',
+// 		markers: true,
+// 		scrub: true,
+// 	},
+// });
