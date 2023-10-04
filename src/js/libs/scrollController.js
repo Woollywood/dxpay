c6413be0c19b4d811cfc48687af4fa798ef0cee8
woollywood <Gerbert.Wells969@yandex.ru>
2023-10-04 21:48:34 +0300

    animationHandler v1

--- src/js/libs/scrollController.js
+++ src/js/libs/scrollController.js
@@ -13,35 +13,148 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-const loadAnimation = (file) => {
-	return new Promise((resolve, reject) => {
-		const animation = bodymovin.loadAnimation({
-			container: document.getElementById('animation-layout'),
-			renderer: 'svg',
-			loop: true,
-			autoplay: true,
-			path: file,
+class AnimationHandler {
+	constructor(layout, meta) {
+		this._layout = layout;
+		this._meta = meta;
+
+		this.layoutObserver = new MutationObserver(this._layoutObserverCallback.bind(this));
+		this.layoutObserver.observe(this._layout, {
+			childList: true,
 		});
 
-		animation.addEventListener('DOMLoaded', () => {
-			console.log('loaded');
-			resolve(animation);
-		});
+		this._currentAnimationIndex = -1;
+		this._animationCache = new Map();
+	}
 
-		animation.addEventListener('error', (error) => {
-			reject(error);
-		});
-	});
-};
+	_layoutObserverCallback(event) {
+		for (const { addedNodes } of event) {
+			for (const node of addedNodes) {
+				if (node.tagName === 'svg') node.dataset.animationId = this._currentAnimationIndex;
+			}
+		}
+	}
 
-const PATH = '../../files/lottie/data.json';
+	_loadAnimation(path) {
+		return new Promise((resolve, reject) => {
+			const animation = bodymovin.loadAnimation({
+				container: this._layout,
+				renderer: 'svg',
+				loop: false,
+				autoplay: false,
+				path,
+			});
+
+			animation.addEventListener('DOMLoaded', () => {
+				resolve(animation);
+			});
+
+			animation.addEventListener('error', (error) => {
+				reject(error);
+			});
+		});
+	}
+
+	_nextAnimationStylesReset() {
+		const animationSvgList = this._layout.querySelectorAll('svg');
+		animationSvgList?.forEach((animation) => animation.classList.remove('animation-playing'));
+		const animationSvg = this._layout.querySelector(`[data-animation-id="${this._currentAnimationIndex}"]`);
+		animationSvg.classList.add('animation-playing');
+	}
+
+	nextAnimation() {
+		this._currentAnimationIndex++;
+		let animationObject = null;
+		if (!this._animationCache.has(this._meta[this._currentAnimationIndex].src)) {
+			this._loadAnimation(this._meta[this._currentAnimationIndex].src).then((animation) => {
+				animationObject = animation;
+				this._nextAnimationStylesReset();
+				this._animationCache.set(this._meta[this._currentAnimationIndex].src, animation);
+				animationObject.setDirection(1);
+				animationObject.play();
+			});
+		} else {
+			animationObject = this._animationCache.get(this._meta[this._currentAnimationIndex].src);
+			this._nextAnimationStylesReset();
+			this._animationCache.get(this._meta[this._currentAnimationIndex].src).goToAndPlay(0, true);
+
+			animationObject.setDirection(1);
+			animationObject.play();
+		}
+	}
+
+	prevAnimation() {
+		const animationObject = this._animationCache.get(this._meta[this._currentAnimationIndex].src);
+		animationObject.setDirection(-1);
+		animationObject.play();
+
+		this._currentAnimationIndex--;
+	}
+}
+
+function returnAnimationFiles() {
+	return [
+		{
+			src: './files/lottie/data-01.json',
+		},
+		{
+			src: './files/lottie/data-02.json',
+		},
+		{
+			src: './files/lottie/data-03.json',
+		},
+		{
+			src: './files/lottie/data-04.json',
+		},
+		{
+			src: './files/lottie/data-05.json',
+		},
+		{
+			src: './files/lottie/data-06.json',
+		},
+		{
+			src: './files/lottie/data-07.json',
+		},
+		{
+			src: './files/lottie/data-08.json',
+		},
+		{
+			src: './files/lottie/data-09.json',
+		},
+		{
+			src: './files/lottie/data-10.json',
+		},
+		{
+			src: '../../files/lottie/data-11.json',
+		},
+	];
+}
 
 async function initLottie() {
 	if (window.innerWidth > 767.98) {
 		const sections = document.querySelectorAll('.section');
 		sections.forEach((section, index) => (section.id = `section-${index}`));
 
-		loadAnimation(PATH);
+		let animationHandler = new AnimationHandler(
+			document.getElementById('animation-layout'),
+			returnAnimationFiles()
+		);
+		animationHandler.nextAnimation();
+		sections.forEach((section, index) => {
+			ScrollTrigger.create({
+				trigger: section,
+				start: 'top top',
+				// markers: true,
+				onEnter: (triggerEvent) => {
+					console.log('scroll into section');
+					animationHandler.nextAnimation();
+				},
+				onLeaveBack: (triggerEvent) => {
+					console.log('leave section');
+					animationHandler.prevAnimation();
+				},
+			});
+		});
 	}
 }
 
