8f167edf30352cbeacbcfc56827e8e19112637bf
woollywood <Gerbert.Wells969@yandex.ru>
2023-10-04 23:23:06 +0300

    animation stack

--- src/js/libs/scrollController.js
+++ src/js/libs/scrollController.js
@@ -24,13 +24,17 @@ class AnimationHandler {
 		});
 
 		this._currentAnimationIndex = -1;
+		this._animationObserverIndex = 0;
+
 		this._animationCache = new Map();
+		this._stack = [];
 	}
 
 	_layoutObserverCallback(event) {
 		for (const { addedNodes } of event) {
 			for (const node of addedNodes) {
-				if (node.tagName === 'svg') node.dataset.animationId = this._currentAnimationIndex;
+				console.log(this._stack);
+				if (node.tagName === 'svg') node.dataset.animationId = this._animationObserverIndex++;
 			}
 		}
 	}
@@ -49,44 +53,68 @@ class AnimationHandler {
 				resolve(animation);
 			});
 
+			animation.addEventListener('complete', (e) => {
+				console.log('complete');
+
+				if (this._stack.length) {
+					this._animationPlay(this._stack.shift());
+				}
+			});
+
 			animation.addEventListener('error', (error) => {
 				reject(error);
 			});
 		});
 	}
 
-	_nextAnimationStylesReset() {
+	_svgStylesReset(animationIndex) {
 		const animationSvgList = this._layout.querySelectorAll('svg');
 		animationSvgList?.forEach((animation) => animation.classList.remove('animation-playing'));
-		const animationSvg = this._layout.querySelector(`[data-animation-id="${this._currentAnimationIndex}"]`);
+		const animationSvg = this._layout.querySelector(`[data-animation-id="${animationIndex}"]`);
 		animationSvg.classList.add('animation-playing');
 	}
 
-	nextAnimation() {
-		this._currentAnimationIndex++;
+	/**
+	 * Воспроизведение анимации по индексу
+	 */
+	_animationPlay(animationIndex) {
 		let animationObject = null;
-		if (!this._animationCache.has(this._meta[this._currentAnimationIndex].src)) {
-			this._loadAnimation(this._meta[this._currentAnimationIndex].src).then((animation) => {
+		if (!this._animationCache.has(this._meta[animationIndex].src)) {
+			this._loadAnimation(this._meta[animationIndex].src).then((animation) => {
 				animationObject = animation;
-				this._nextAnimationStylesReset();
-				this._animationCache.set(this._meta[this._currentAnimationIndex].src, animation);
+				this._svgStylesReset(animationIndex);
+				this._animationCache.set(this._meta[animationIndex].src, animation);
 				animationObject.setDirection(1);
 				animationObject.play();
 			});
 		} else {
-			animationObject = this._animationCache.get(this._meta[this._currentAnimationIndex].src);
-			this._nextAnimationStylesReset();
-			this._animationCache.get(this._meta[this._currentAnimationIndex].src).goToAndPlay(0, true);
-
+			animationObject = this._animationCache.get(this._meta[animationIndex].src);
+			this._svgStylesReset(animationIndex);
 			animationObject.setDirection(1);
 			animationObject.play();
 		}
 	}
 
+	/**
+	 * Воспроизведение следующей анимации или добавление в очередь
+	 */
+	nextAnimation() {
+		this._currentAnimationIndex++;
+
+		if (!this._stack.length) {
+			this._animationPlay(this._currentAnimationIndex);
+			this._stack.push(this._currentAnimationIndex);
+		} else {
+			this._stack.push(this._currentAnimationIndex);
+		}
+	}
+
+	
 	prevAnimation() {
 		const animationObject = this._animationCache.get(this._meta[this._currentAnimationIndex].src);
 		animationObject.setDirection(-1);
 		animationObject.play();
+		this._svgStylesReset();
 
 		this._currentAnimationIndex--;
 	}
@@ -146,7 +174,7 @@ async function initLottie() {
 				start: 'top top',
 				// markers: true,
 				onEnter: (triggerEvent) => {
-					console.log('scroll into section');
+					console.log(`scroll into section ${section.id}`);
 					animationHandler.nextAnimation();
 				},
 				onLeaveBack: (triggerEvent) => {
