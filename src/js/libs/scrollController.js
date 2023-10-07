607584f517ac3c356701f3952895804fc0c34f95
woollywood <Gerbert.Wells969@yandex.ru>
2023-10-07 16:48:03 +0300

    mobile scroll animation

--- src/js/libs/scrollController.js
+++ src/js/libs/scrollController.js
@@ -42,11 +42,17 @@ class AnimationHandlerMobile {
 	}
 
 	async _load(animationIndex, container) {
-		let animation = await this._loadAnimation(this._meta[animationIndex].src, container);
+		let animation = await this._loadAnimation(
+			this._meta[animationIndex].src,
+			container,
+			this._meta[animationIndex].startOffset,
+			this._meta[animationIndex].endOffset
+		);
+		animation.goToAndStop(this._meta[animationIndex].startOffset, true);
 		this._animationList.set(this._meta[animationIndex].src, animation);
 	}
 
-	_loadAnimation(path, container) {
+	_loadAnimation(path, container, offset) {
 		return new Promise((resolve, reject) => {
 			const animation = bodymovin.loadAnimation({
 				container,
@@ -71,7 +77,9 @@ class AnimationHandlerMobile {
 	}
 
 	play(animationIndex) {
-		this._animationList.get(this._meta[animationIndex].src).play();
+		this._animationList
+			.get(this._meta[animationIndex].src)
+			.playSegments([this._meta[animationIndex].startOffset, this._meta[animationIndex].endOffset], true);
 	}
 }
 
@@ -86,7 +94,7 @@ function loadAnimation(path, container) {
 		});
 
 		animation.addEventListener('enterFrame', (e) => {
-			// console.log(e);
+			console.log(e);
 		});
 
 		animation.addEventListener('DOMLoaded', () => {
@@ -113,14 +121,20 @@ async function initLottie() {
 	let animationWrapper = document.querySelector('#animation-wrapper');
 
 	if (window.innerWidth > 767.98) {
+		if (lenis.targetScroll === 0) {
+			document.documentElement.classList.add('lock');
+			lenis.stop();
+		}
+
 		let animation = null;
 		let animationFrames = null;
 
 		ScrollTrigger.create({
 			trigger: animationLayout,
-			start: '-1px top',
+			start: '1px top',
 			end: 'bottom bottom',
 			onEnter: (triggerEvent) => {
+				console.log('enter');
 				loadAnimation(PATH, animationWrapper).then((animationObject) => {
 					animation = animationObject;
 					animationFrames = animationObject.totalFrames;
@@ -139,7 +153,6 @@ async function initLottie() {
 				trigger: section,
 				start: '-40% top',
 				onEnter: (triggerEvent) => {
-					console.log('enter');
 					animationHandler.play(index);
 				},
 				onLeaveBack: (triggerEvent) => {},
@@ -152,37 +165,53 @@ function returnAnimationFiles() {
 	return [
 		{
 			src: './files/lottie/data-01.json',
+			startOffset: 0,
+			endOffset: 74,
 		},
 		{
 			src: './files/lottie/data-02.json',
-			offset: 76,
+			startOffset: 74,
+			endOffset: 166,
 		},
 		{
 			src: './files/lottie/data-03.json',
+			startOffset: 166,
+			endOffset: 300,
 		},
 		{
 			src: './files/lottie/data-04.json',
+			startOffset: 300,
+			endOffset: 400,
 		},
 		{
 			src: './files/lottie/data-05.json',
+			startOffset: 400,
+			endOffset: 500,
 		},
 		{
 			src: './files/lottie/data-06.json',
+			startOffset: 500,
+			endOffset: 600,
 		},
 		{
 			src: './files/lottie/data-07.json',
+			startOffset: 600,
+			endOffset: 660,
 		},
 		{
 			src: './files/lottie/data-08.json',
+			startOffset: 660,
+			endOffset: 860,
 		},
 		{
 			src: './files/lottie/data-09.json',
+			startOffset: 860,
+			endOffset: 960,
 		},
 		{
 			src: './files/lottie/data-10.json',
-		},
-		{
-			src: './files/lottie/data-11.json',
+			startOffset: 960,
+			endOffset: 1120,
 		},
 	];
 }
